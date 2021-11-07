fab98a97215a11c7b720cd6eecdea25c09a1600b
AgnethaBaltz <202105119@post.au.dk>
2021-11-07 13:18:39 +0100

    Implemented

--- src/sum.c
+++ src/sum.c
@@ -2,5 +2,12 @@
 
 int sum(int a[], int n)
 {
-    return 0;
+    //If the array is empty 
+    if (n == 0) {
+        return 0;
+        //If the array is of length n
+    } else if (n > 0) {
+        //taking the sum of each array element
+        return a[n - 1] + sum(a, n - 1);
+    } 
 }
