8d3a43de2ca51a9016394e23ed41513f6a9768c8
AgnethaBaltz <202105119@post.au.dk>
2021-11-07 13:25:01 +0100

    implemented2.0

--- src/search.c
+++ src/search.c
@@ -1,12 +1,17 @@
+/* Agnetha Baltz
+This program uses a recrusive function that searches the integer array a for the element x.
+*/
 #include "search.h"
 #include <stdbool.h>
 
 bool search(int a[], int n, int x)
-{
+{   //If the array consists of elements, at the searched element has been found
     if(n > 0 && a[n - 1] == x) {
         return true;
+     //If the array consists element, but the searched element hasn't been found jet.
     } else if (n > 0 && a[n - 1] != x) {
         return search(a, n - 1, x);
+     //If there are no elements in the array
     } else if (n == 0){
         return false;
     }    
